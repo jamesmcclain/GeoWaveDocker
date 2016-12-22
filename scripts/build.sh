3af5848a49649a5b41d9440a4924215170c938f7
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-12-22 09:42:40 -0500

    Get `geowave-image` Target Working

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,12 +6,11 @@ cd /geowave
 
 set -x
 mvn clean > /dev/null 2>&1
-mvn install $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
-mvn package -P accumulo-container-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
-mvn package -P geotools-container-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
-mvn package -P geowave-analytic-mapreduce $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
-mvn package -P geowave-tools-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
-mvn package -P geotools-container-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn install -T 1.618C $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn package -T 1.618C -P accumulo-container-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn package -T 1.618C -P geowave-analytic-mapreduce $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn package -T 1.618C -P geowave-tools-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
+mvn package -T 1.618C -P geotools-container-singlejar $EXTRA_ARGS $BUILD_ARGS > /dev/null 2>&1
 set +x
 
 chown -R $1:$2 /root/.m2
